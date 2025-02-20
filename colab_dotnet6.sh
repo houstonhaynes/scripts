7fadbd2ca7b26815d02312eaf0d7f7f84156449c
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2025-02-20 13:53:40 -0500

    update to SDK6.136

--- colab_dotnet6.sh
+++ colab_dotnet6.sh
@@ -3,7 +3,7 @@
 # Atilim Gunes Baydin (gunes@robots.ox.ac.uk), February 2022
 # Houston Haynes (h3@ad4s.co), February 2025
 
-echo "Installing dotnet-sdk-6.0.428 and dotnet interactive 1.0.355307..."
+echo "Installing dotnet-sdk-6.0.136 and dotnet interactive 1.0.355307..."
 
 # Get Ubuntu version
 source /etc/os-release
@@ -19,11 +19,11 @@ apt-get install -y dotnet-host dotnet-hostfxr-6.0 dotnet-runtime-6.0 aspnetcore-
 apt-get install -y dotnet-sdk-6.0
 
 # Create version directory structure
-mkdir -p /usr/share/dotnet/host/fxr/6.0.428
+mkdir -p /usr/share/dotnet/host/fxr/6.0.136
 
 # Set up symlinks
 ln -s /usr/lib/dotnet/dotnet /usr/share/dotnet/dotnet
-ln -s /usr/lib/dotnet/host/fxr/6.0.428 /usr/share/dotnet/host/fxr/6.0.428
+ln -s /usr/lib/dotnet/host/fxr/6.0.136 /usr/share/dotnet/host/fxr/6.0.136
 
 # Install dotnet interactive
 dotnet tool install -g Microsoft.dotnet-interactive --version 1.0.355307
@@ -31,6 +31,10 @@ dotnet tool install -g Microsoft.dotnet-interactive --version 1.0.355307
 # Add dotnet tools to PATH
 export PATH=$PATH:$HOME/.dotnet/tools
 dotnet interactive jupyter install
+
+# Create kernel configurations
+echo "{\"argv\": [\"$HOME/.dotnet/tools/dotnet-interactive\", \"jupyter\", \"--default-kernel\", \"fsharp\", \"--http-port-range\", \"1000-3000\", \"{connection_file}\"], \"display_name\": \".NET (F#)\", \"language\": \"F#\"}" > /root/.local/share/jupyter/kernels/fsharp/kernel.json
+echo "{\"argv\": [\"$HOME/.dotnet/tools/dotnet-interactive\", \"jupyter\", \"--default-kernel\", \"csharp\", \"--http-port-range\", \"1000-3000\", \"{connection_file}\"], \"display_name\": \".NET (C#)\", \"language\": \"C#\"}" > /root/.local/share/jupyter/kernels/csharp/kernel.json
 
 echo "Done."
 echo "Select \"Runtime\" -> \"Change Runtime Type\" and click \"Save\" to activate for this notebook"
