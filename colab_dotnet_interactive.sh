18cebf98f6686035a4a9affa6df26d78764d799d
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2025-02-20 16:25:33 -0500

    remove symlink

--- colab_dotnet_interactive.sh
+++ colab_dotnet_interactive.sh
@@ -13,8 +13,7 @@ dpkg -i packages-microsoft-prod.deb
 # Update package list and install .NET SDK packages
 apt-get update
 apt-get install -y dotnet-sdk-6.0 aspnetcore-runtime-6.0 dotnet-runtime-6.0 dotnet-hostfxr-6.0
-sudo mkdir -p /usr/share/dotnet/host
-sudo ln -s /usr/share/dotnet/shared/Microsoft.NETCore.App /usr/share/dotnet/host/fxr
+
 
 # After installing .NET SDK
 if [ ! -d "/usr/share/dotnet" ]; then
