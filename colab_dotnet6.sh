e404ae3edb4330447727b9c52521c3cbce0acc36
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2025-02-20 13:34:48 -0500

    update per version pin of .NET interactive

--- colab_dotnet6.sh
+++ colab_dotnet6.sh
@@ -2,12 +2,13 @@
 # Adds Jupyter kernels for F# and C# to a Google Colab session
 # Atilim Gunes Baydin (gunes@robots.ox.ac.uk), February 2022
 # Houston Haynes (h3@ad4s.co), February 2025
+
 echo "Installing dotnet-sdk-6.0 and dotnet interactive..."
 wget -q https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb > /dev/null
 apt-get update > /dev/null
 apt-get install dotnet-sdk-6.0 > /dev/null
-dotnet tool install -g Microsoft.dotnet-interactive > /dev/null
+dotnet tool install -g Microsoft.dotnet-interactive --version 1.0.355307 > /dev/null
 export PATH=$PATH:$HOME/.dotnet/tools
 dotnet interactive jupyter install > /dev/null
 echo "{\"argv\": [\"$HOME/.dotnet/tools/dotnet-interactive\", \"jupyter\", \"--default-kernel\", \"fsharp\", \"--http-port-range\", \"1000-3000\", \"{connection_file}\"], \"display_name\": \".NET (F#)\", \"language\": \"F#\"}" > /root/.local/share/jupyter/kernels/fsharp/kernel.json
